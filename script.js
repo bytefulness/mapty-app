8487945111c110bf35a4cc4c331a1ea6615abfa3
eakyel <e.akyel@outlook.com>
2021-02-23 20:37:24 +0300

    Access User Location
    Using geolocation api taken user current location. Added Leaflet Javascript Library to use interactive map for functionality of the application.

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Access User Location
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    // When success getting location
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+
+    // When fail getting location
+    function () {
+      alert('Could not get position. Please check the location permission.');
+    }
+  );
+}
