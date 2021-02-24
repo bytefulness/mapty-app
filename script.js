6aac25ff6c347ec667e69a4e2bd2972b7cea7b36
eakyel <e.akyel@outlook.com>
2021-02-24 08:08:47 +0300

    Render Workout Input Form
    Create functionality: When user click on map open input form for user will be able to enter workout information.

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 // Access User Location
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -21,29 +23,19 @@ if (navigator.geolocation) {
       const coords = [latitude, longitude];
 
       // Leatlef Library
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on('click', function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
+      map.on('click', function (mapE) {
+        // Copy event object to variable
+        mapEvent = mapE;
 
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxwidth: 150,
-              minwidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Workout')
-          .openPopup();
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
 
@@ -53,3 +45,30 @@ if (navigator.geolocation) {
     }
   );
 }
+
+// Event Listener for Form
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
+    '';
+
+  inputCadence.blur();
+  inputElevation.blur();
+
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxwidth: 150,
+        minwidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
